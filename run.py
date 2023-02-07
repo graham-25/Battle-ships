4711273755019ddf9ae75ef7d5ccd5a97fe2eae2
graham-25 <grbills29@googlemail.com>
2023-02-07 16:47:16 +0000

    Initial Commit

--- run.py
+++ run.py
@@ -1,3 +1,10 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+board_size =  input("Enter the board size")
+user_board = [(' ') * board_size for x in range(board_size)]
+com_board = [(' ') * board_size for x in range(board_size)]
+
+build_board(board_size):
+
