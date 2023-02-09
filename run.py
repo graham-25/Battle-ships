2c5e5d260440459ae24b0103c42f55fdd55b7a5b
graham-25 <grbills29@googlemail.com>
2023-02-09 22:55:31 +0000

    Made my first function where the computer will ask the user for the board size and it makes a squaired array where the ships will be hidden inside.

--- run.py
+++ run.py
@@ -2,9 +2,27 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
-board_size =  input("Enter the board size")
-user_board = [(' ') * board_size for x in range(board_size)]
-com_board = [(' ') * board_size for x in range(board_size)]
+def start_game():
+    """
+    This is where the board size is defined by the user, 
+    it takes the user's input as the board size squaired. 
+    We are going to initialise 2 boards so that one can 
+    be hidden to hide the ships to be destroyed.
+    """
 
-build_board(board_size):
+    board_size =  int(input("Please enter the board size! "))
+    size = board_size ** 2
+    user_board = [(" ") for x in range(size)]
+    com_board = [(" ") for x in range(size)]
 
+    return user_board
+
+def main():
+    """
+    
+    """
+    game = start_game()
+
+print("Welcome to Battle Ships")
+print("The aim of this game is to guess where the computers ships are to Destroy them. ")
+main()
